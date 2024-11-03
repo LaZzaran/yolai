ea3747f48700630ed83aea2abe9e1bf2707c32eb
nupeldaOruc <154071797+nupeldaOruc@users.noreply.github.com>
2024-11-03 21:54:01 +0300

    Add files via upload

---
+++ Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿// Models/Subject.cs
+namespace BTK.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string SubjectName { get; set; }
+        public string LessonName { get; set; }
+        public string Image { get; set; }
+        public LessonType LessonType { get; set; }
+    }
+
+    public enum LessonType
+    {
+        Matematik = 1,
+        Turkce = 2,
+        Fen = 3,
+        Sosyal = 4
+    }
+}
